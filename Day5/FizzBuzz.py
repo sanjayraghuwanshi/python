5b10bddf3d1b951c41cc392cb382f8d7bd045c90
Sanjay Raghuwanshi <raghu1c.sanjay@gmail.com>
2022-10-25 18:34:54 +0530

    Day5

---
+++ Day5/FizzBuzz.py
@@ -0,0 +1,17 @@
+
+# Code to print Fizz Buzz in place of numbers when :
+#  i % 3 == 0 ; print Fizz 
+#  i % 5 == 0 ; print Buzz  
+#  i % 3 == 0 and i % 5 == 0 ; print FizBuzz
+# https://en.wikipedia.org/wiki/Fizz_buzz
+
+for i in range(1, 101, 1):
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0 or i % 5 == 0:
+        if i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+    else:
+        print(i)
