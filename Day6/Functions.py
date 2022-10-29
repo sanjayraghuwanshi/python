717b82950630ab4411a2eac46e2a1f42b581885b
Sanjay Raghuwanshi <raghu1c.sanjay@gmail.com>
2022-10-29 19:46:37 +0530

    Day6

---
+++ Day6/Functions.py
@@ -0,0 +1,15 @@
+# https://docs.python.org/3/library/functions.html
+
+# Defining functions 
+
+# Starts with def <function name ()>
+
+def my_function():
+    # do this.
+    # do this.
+    # Finally do this.
+    print("Hello")
+    print("Bye")
+
+my_function() # To call the function, put the input in parenthesis if required, above function doesn't require any.
+
