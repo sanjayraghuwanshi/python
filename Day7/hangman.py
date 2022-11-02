9d7ec1096670c4c53446b932b1989d2e60fca583
Sanjay Raghuwanshi <sraghuwanshi@vectra.ai>
2022-11-02 23:31:43 +0530

    Day7

--- Day7/hangman.py
+++ Day7/hangman.py
@@ -1,10 +1,21 @@
 import random
 import hangman_words
-import hangman_art
+import hangman_art          # or -> from hangman_art import logo, stages
+from os import system, name
 
 # print(stages[6]) - start
 # print(stages[0]) - game over
 
+def clear():
+ 
+    # for windows
+    if name == 'nt':
+        _ = system('cls')
+ 
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = system('clear')
+        
 print(hangman_art.logo)
 
 chosen_word = random.choice(hangman_words.word_list)
@@ -17,19 +28,17 @@ print(f'Pssst, the solution is {chosen_word}.')
 display = []
 for _ in chosen_word:
         display += "_"
-#print(display)
 
 end_of_game = False
 lives = 6
-list_of_guesses = []
 
 # Loop through chosen word for each guess
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
-    if guess in list_of_guesses:
+    clear()
+    if guess in display:
         print(f"You've already guessed {guess}")
-    list_of_guesses.append(guess)
-    
+   
     for position in range(word_length):
         letter = chosen_word[position]
         if letter == guess:
